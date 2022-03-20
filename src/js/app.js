f80bd4cccae9552e42883b533573b81100f87b0e
Ankit Tanna <atanna@slb.com>
2022-03-20 15:02:20 +0530

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 if (module.hot) {
   module.hot.accept();
 }
+
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+  // modal.show();
+  modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+  modal.close();
+});
